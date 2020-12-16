ef880f6409646721a03e616ac8170da5891af480
Apoorva1.Pandey <apoorva1.pandey@ril.com>
2020-12-16 14:06:15 +0530

    ADDED getall users operation

--- controllers.go
+++ controllers.go
@@ -8,9 +8,9 @@ import (
 	"net/http"
 
 	
-	/*"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"*/
+	//"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var userCollection = db().Database("goTest").Collection("userinfo")
@@ -35,3 +35,25 @@ log.Fatal(err)
 json.NewEncoder(w).Encode(insertResult.InsertedID) 
 }
 
+func getAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var results []primitive.M                                   //slice for multiple documents
+	cur, err := userCollection.Find(context.TODO(), bson.D{{}}) //returns a *mongo.Cursor
+	if err != nil {
+
+		fmt.Println(err)
+
+	}
+	for cur.Next(context.TODO()) { //Next() gets the next document for corresponding cursor
+
+		var elem primitive.M
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, elem) // appending document pointed by Next()
+	}
+	cur.Close(context.TODO()) // close the cursor once stream of documents has exhausted
+	json.NewEncoder(w).Encode(results)
+}
--- main.go
+++ main.go
@@ -3,18 +3,20 @@ import (
 "github.com/gorilla/mux"
 "log"
 "net/http"
+
 )
 
 func main() {
 s := mux.NewRouter()
 
-//Routes
+//Routesgo 
 s.HandleFunc("/createProfile", createProfile).Methods("POST")
-//s.HandleFunc("/getAllUsers", getAllUsers).Methods("GET")
+
+s.HandleFunc("/getAllUsers", getAllUsers).Methods("GET")
 
 //s.HandleFunc("/updateProfile", updateProfile).Methods("PUT")
 
 log.Fatal(http.ListenAndServe(":8000", s)) 
 }
 
-//run the program as -- go run main.go db.go controller.go
+//run the program as -- go run main.go db.go controllers.go
